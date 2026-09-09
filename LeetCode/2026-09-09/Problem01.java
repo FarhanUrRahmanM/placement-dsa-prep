4cf1ac806ed87c2c55aae1b4346b238151c36b10
farhan0717 <YOUR_GITHUB_EMAIL>
2026-09-09 11:45:55 +0530

    Add first two LeetCode solutions

---
+++ LeetCode/2026-09-09/Problem01.java
@@ -0,0 +1,16 @@
+
+
+public class Problem01 { 
+    public long countCommas(long n) {
+        long p = 1000, res = 0;
+
+        while (p <= n) {
+            res += n - p + 1;
+            p *= 1000;
+        }
+
+        return res;
+    }
+
+    
+}
