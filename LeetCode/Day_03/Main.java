2305e1e5f41694563dada09ee7f8c20bff864799
FarhanUrRahmanM <farhanurrahman26@gmail.com>
2026-09-11 13:58:08 +0530

    test: add invalid BST test case

--- LeetCode/Day_03/Main.java
+++ LeetCode/Day_03/Main.java
@@ -7,5 +7,13 @@ public class Main {
         root.left = new TreeNode(1);
         root.right = new TreeNode(3);
         System.out.println("Test 1 (Valid): " + solver.isValidBST(root));
+        
+        // Test 2: Invalid BST
+        TreeNode root2 = new TreeNode(5);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(4);
+        root2.right.left = new TreeNode(3);
+        root2.right.right = new TreeNode(6);
+        System.out.println("Test 2 (Invalid): " + solver.isValidBST(root2));
     }
 }
