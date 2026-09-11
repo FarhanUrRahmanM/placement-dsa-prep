891878865e7be3c70d8e76018e2240f50ef558d0
FarhanUrRahmanM <farhanurrahman26@gmail.com>
2026-09-11 13:58:07 +0530

    chore: add TreeNode class for local compilation

--- LeetCode/Day_03/Solution1.java
+++ LeetCode/Day_03/Solution1.java
@@ -1,3 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution1 {
     public boolean isValidBST(TreeNode root) {
         return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
