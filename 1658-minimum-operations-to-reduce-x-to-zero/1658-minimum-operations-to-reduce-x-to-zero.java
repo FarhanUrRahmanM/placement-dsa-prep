4b4d3a586e0e3c936ffce58212e325fc41cd8863
FarhanUrRahmanM <farhanurrahman26@gmail.com>
2026-09-23 10:28:35 +0530

    Time: 4 ms (98.07%), Space: 102.3 MB (29.35%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -13,7 +13,7 @@ class Solution {
 
         int left = 0;
         int sum = 0;
-        int maxLength = -1;
+        int maxLen = -1;
 
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
@@ -24,14 +24,10 @@ class Solution {
             }
 
             if (sum == target) {
-                maxLength = Math.max(maxLength, right - left + 1);
+                maxLen = Math.max(maxLen, right - left + 1);
             }
         }
 
-        if (maxLength == -1) {
-            return -1;
-        }
-
-        return nums.length - maxLength;
+        return maxLen == -1 ? -1 : nums.length - maxLen;
     }
 }
