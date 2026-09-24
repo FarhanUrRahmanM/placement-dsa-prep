d24c85504d7459db4d0a8ce128f7765c5a602498
FarhanUrRahmanM <farhanurrahman26@gmail.com>
2026-09-24 09:17:00 +0530

    Add Course Schedule solution

---
+++ codes/CourseSchedule.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int prerequisiteCourse = prerequisite[1];
+
+            graph.get(prerequisiteCourse).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int completed = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            completed++;
+
+            for (int next : graph.get(current)) {
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return completed == numCourses;
+    }
+}
